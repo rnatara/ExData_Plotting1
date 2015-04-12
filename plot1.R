8fb55dc2edc6a3cfc474e49523a55c20743986b3
Ramesh Natarajan <rnatara@gmail.com>
2015-04-12 15:17:01 -0400

    script and png for exploratory data analysis project 1

---
+++ plot1.R
@@ -0,0 +1,41 @@
+# plot1.R
+#-----------------------------------------------------------------------------
+# Draw frequency of Gloabl Active Power for 2 days (2007-02-01 to 2007-02-02) 
+# Rawdata: Electric Power Consumption Data 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Project sumbission for Exploratory Data Analysis Data Sciences Course
+# *** Note ***
+#skip and nrows in read.csv obtained by grep/wc and adjusting for header row
+#echo "skip: "
+#grep -n '^1/2/2007' household_power_consumption.txt |head -1|cut -f1 -d:
+#echo "nrows: "
+#egrep '^[12]/2/2007' household_power_consumption.txt |wc -
+#skip:
+#66638
+#nrows:
+#2880
+#-----------------------------------------------------------------------------
+# nram, Apr 12, 2015
+
+setwd("c:/Users/rnatarajan/workspace/Courcera/DataScience/ExploratoryDataAnalysis/Project")
+
+#read partial data into memory
+data<-read.csv("household_power_consumption.txt", 
+	       sep=";", 
+	       skip=66636, 
+	       nrows=2880, 
+	       col.names=c("Date", "Time", "Global_active_power", 
+			   "Global_reactive_power", "Voltage", 
+			   "Global_intensity", "Sub_metering_1", 
+			   "Sub_metering_2", "Sub_metering_3"))
+
+# draw freq of Global Active Power for 2 days; output to png device
+png(file="plot1.png", width=480, height=480)
+hist(data$Global_active_power, col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+dev.off()
+
+# optional 
+#rug(data$Global_active_power)
+#View(data,"Global Active Power (2 days)")
---
+++ plot4.R
@@ -0,0 +1,58 @@
+# plot3.R
+#-----------------------------------------------------------------------------
+# Draw frequency of Gloabl Active Power for 2 days (2007-02-01 to 2007-02-02) 
+# Raw Data: Electric Power Consumption Data 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Project sumbission for Exploratory Data Analysis Data Sciences Course
+# *** Note ***
+#skip and nrows in read.csv obtained by grep/wc and adjusting for header row
+#echo "skip: "
+#grep -n '^1/2/2007' household_power_consumption.txt |head -1|cut -f1 -d:
+#echo "nrows: "
+#egrep '^[12]/2/2007' household_power_consumption.txt |wc -
+#skip:
+#66638
+#nrows:
+#2880
+#-----------------------------------------------------------------------------
+# nram, Apr 12, 2015
+
+setwd("c:/Users/rnatarajan/workspace/Courcera/DataScience/ExploratoryDataAnalysis/Project")
+
+#read partial data into memory
+data<-read.csv("household_power_consumption.txt", 
+	       sep=";", 
+	       skip=66636, 
+	       nrows=2880, 
+	       col.names=c("Date", "Time", "Global_active_power", 
+			   "Global_reactive_power", "Voltage", 
+			   "Global_intensity", "Sub_metering_1", 
+			   "Sub_metering_2", "Sub_metering_3"))
+
+# Get DateTime from Date and Time columns
+data$datetime<- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+# draw plots to png graphic device
+png(file="plot4.png", width=480, height=480)
+attach(data)
+par(mfrow=c(2,2))
+  # Plot 1
+  plot(datetime, Global_active_power, type="l",
+     xlab="",ylab="Global Active Power (kilowatts)")
+
+  # Plot 2
+  plot(datetime, Voltage, type="l",
+       xlab="",ylab="Voltage")
+
+  # Plot 3
+  plot(datetime, Sub_metering_1, type="l", col="black",
+     xlab="", ylab="Energy Sub metering")
+  lines(datetime, Sub_metering_2, type="l", col="red")
+  lines(datetime, Sub_metering_3, type="l", col="blue")
+  legend("topright",  box.col="white", inset=0.01, lty=c(1,1,1), col=c("black","red","blue"), 
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+  # Plot 4
+  plot(datetime, Global_reactive_power, type="l")
+  detach(data)
+dev.off()
